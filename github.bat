4af81ce754d73535e101225f5ae20539cac0de0e
unknown <itbmaculture@gmail.com>
2025-01-23 10:33:04 +0700

    updata boss

--- github.bat
+++ github.bat
@@ -4,3 +4,9 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/itbmaculture/webmap.git
 git push -u origin main
+
+
+rem …or push an existing repository from the command line
+rem git remote add origin https://github.com/itbmaculture/webmap.git
+rem git branch -M main
+rem git push -u origin main
